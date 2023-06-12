c4237adc32ca4feb99c6bcc8d6b487311a6d62b0
JonathanDavidWise <107775956+JonathanDavidWise@users.noreply.github.com>
2023-06-11 19:55:15 -0500

    final 4hour training

---
+++ python/Chef.py
@@ -0,0 +1,10 @@
+class Chef:
+
+    def make_chicken(self):
+        print("Chicken")
+
+    def make_salade(self):
+        print("Salad")
+
+    def make_special_dish(sself):
+        print("Special")
